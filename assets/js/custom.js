6afabba6844cc3b66c63801f4fb6d5c95c1ed6d1
Magnus Westhofen <m.westhofen@12-05.de>
2021-09-09 14:56:12 +0200

    align center

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,7 @@
 "use strict";
 
 jQuery(function ($) {
-	$(".hamburger, .overlay").click(function () {
-		$("body").toggleClass("menu-show");
-	});
-	ScrollReveal().reveal(".row > *", { reset: true });
+  $(".hamburger, .overlay").click(function () {
+    $('body').toggleClass("menu-show");
+  });
 });
