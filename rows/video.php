0625366756eebdb6f4a8845b5d76fd2b9b050176
Magnus Westhofen <m.westhofen@12-05.de>
2021-09-09 23:05:11 +0200

    autoiplay

--- rows/video.php
+++ rows/video.php
@@ -1,5 +1,5 @@
 <div class="row row-video">
-    <video autoplay loop>
+    <video autoplay muted loop >
         <source src="<?php echo $row['video'];?>" type="video/mp4">
     </video>
     <h2><?php echo $row['headline'];?></h2>
