9f21578667f9bb7d0a34ee737232813fe450e6b3
Magnus Westhofen <m.westhofen@12-05.de>
2022-02-27 00:53:59 +0100

    test

--- inc/mail.view.php
+++ inc/mail.view.php
@@ -9,6 +9,7 @@
                     <th>#</th>
                     <th>Bezeichnung</th>
                     <th>Stück</th>
+                    <th>Preis</th>
                     <th>Gesamt</th>
                 </tr>
             </thead>
@@ -17,8 +18,9 @@
                     <tr>
                         <td><?php echo $item['ID'];?></td>
                         <td><?php echo $item['Name'];?></td>
-                        <td><?php echo $item['Count'];?></td>
-                        <td><?php echo $item['Preis'];?></td>
+                        <td><?php echo $item['count'];?></td>
+                        <td><?php echo $item['Preis'];?> €</td>
+                        <td><?php echo floatval(str_replace(',','.',$item['Preis']))*$item['count'];?></td>
                     </tr>
                 <?php endforeach;endif;?>
             </tbody>
