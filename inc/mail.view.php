e6c65914756273e6307836cd97f0d3e426749887
Magnus Westhofen <m.westhofen@12-05.de>
2022-02-27 00:48:56 +0100

    test

--- inc/mail.view.php
+++ inc/mail.view.php
@@ -1,5 +1,27 @@
 <html>
     <body>
         <h1>Neue Bestellung auf www.hannelise.de</h1>
+        <p>Name: <?php echo $data['name'];?></p>
+        <p>E-Mail: <?php echo $data['email'];?></p>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Bezeichnung</th>
+                    <th>Stück</th>
+                    <th>Gesamt</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if($data['cart']):foreach($data['cart'] as $item):?>
+                    <tr>
+                        <td><?php echo $item['ID'];?></td>
+                        <td><?php echo $item['Name'];?></td>
+                        <td><?php echo $item['Count'];?></td>
+                        <td><?php echo $item['Preis'];?></td>
+                    </tr>
+                <?php endforeach;endif;?>
+            </tbody>
+        </table>
     </body>
 </html>
--- inc/order.service.php
+++ inc/order.service.php
@@ -54,3 +54,17 @@ header("Access-Control-Allow-Headers: *");
 
     }
     new HanneliseOrderService();
+
+
+    function wpb_sender_email( $original_email_address ) {
+        return 'tim.smith@example.com';
+    }
+     
+    // Function to change sender name
+    function wpb_sender_name( $original_email_from ) {
+        return 'Tim Smith';
+    }
+     
+    // Hooking up our functions to WordPress filters 
+    add_filter( 'wp_mail_from', 'wpb_sender_email' );
+    add_filter( 'wp_mail_from_name', 'wpb_sender_name' );
